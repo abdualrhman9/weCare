0ede7e9ee295500170723791fd3dd80e38d94d40
abdoalrhman abaker <abdoalrhman9@hotmail.com>
2021-01-24 01:34:49 +0300

    require sqlite driver on composer.json

--- app/Http/Controllers/SectionController.php
+++ app/Http/Controllers/SectionController.php
@@ -10,7 +10,7 @@ class SectionController extends Controller
 
     public function __construct()
     {
-        $this->middleware("auth:sanctum");
+        // $this->middleware("auth:sanctum");
     }
 
     public function index(){
