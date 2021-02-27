66c3505a6db1c9120976c4a8b1ea5a1c1867c7b8
abdualrhman9 <abdoalrhman9@hotmail.com>
2021-02-26 21:54:28 -0800

    finlize 102

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
+    protected $guarded = [];
     use HasFactory;
     public function report(){
         return $this->belongsTo(Report::class);
